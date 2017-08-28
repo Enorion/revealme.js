df1e9ca5cc8a2cf0795eed918ec731c6d2d0f508
Christopher Kielholz <Enorion@users.noreply.github.com>
2017-08-28 22:38:57 +0200

    Update revealme.js
    Added numbers (zero [0] to nine [9]) and underscore [_] to the replace list

--- revealme.js
+++ revealme.js
@@ -10,7 +10,18 @@ function getEmail(text) {
 
   return text.replace(/ at /g, "@")
              .replace(/ dot /g, ".")
-             .replace(/ hyphen /g, "-");
+             .replace(/ hyphen /g, "-")
+             .replace(/ underscore /g, "_")
+             .replace(/ one /g, "1")
+             .replace(/ two /g, "2")
+             .replace(/ three /g, "3")
+             .replace(/ four /g, "4")
+             .replace(/ five /g, "5")
+             .replace(/ six /g, "6")
+             .replace(/ seven /g, "7")
+             .replace(/ eight /g, "8")
+             .replace(/ nine /g, "9")
+             .replace(/ zero /g, "0");
 }
 
 /**
